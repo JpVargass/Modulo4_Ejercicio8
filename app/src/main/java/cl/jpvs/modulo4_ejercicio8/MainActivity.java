be8051b1b05976f2f9be55dccacb13104b0624f1
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-06-23 15:34:08 -0400

    corregi diseño de botones

--- app/src/main/java/cl/jpvs/modulo4_ejercicio8/MainActivity.java
+++ app/src/main/java/cl/jpvs/modulo4_ejercicio8/MainActivity.java
@@ -2,7 +2,10 @@ package cl.jpvs.modulo4_ejercicio8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListener();
+    }
+
+    private void initListener() {
+        Button btnMain = findViewById(R.id.button_SecondActivity);
+
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity2 = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(activity2);
+            }
+        });
     }
 }
--- app/src/main/java/cl/jpvs/modulo4_ejercicio8/SecondActivity.java
+++ app/src/main/java/cl/jpvs/modulo4_ejercicio8/SecondActivity.java
@@ -2,7 +2,11 @@ package cl.jpvs.modulo4_ejercicio8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        initListener();
+    }
+
+    private void initListener() {
+        Button btnUrl = findViewById(R.id.goToUrl);
+        Button btnBack = findViewById(R.id.button_Back);
+
+        btnUrl.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kotlindoc.blogspot.com/2019/10/android-log-con-timber.html"));
+                startActivity(web);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent activity = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(activity);
+            }
+        });
+
+
     }
 }
